059515504d278eac855fd4b48181217b81bd035d
kyle java <42562960+kylejava@users.noreply.github.com>
2022-01-28 18:47:13 -0800

    Got Firestore To Add & Print Data!!!

--- db.py
+++ db.py
@@ -1,10 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from pprint import pprint
 cred = credentials.Certificate('hoyahacks-339701-firebase-adminsdk-hlqfr-7de4c062b8.json')
 firebase_admin.initialize_app(cred,
 {
-'databaseURL': 'https://hoyahacks.firebaseio.com/'
+'databaseURL': 'https://hoyahacks-339701.firebaseio.com/'
 })
 
 db = firestore.client()
-doc_ref = db.collection(u'applications')
+doc_ref = db.collection(u'users')
+docs = doc_ref.stream()
+
+
+data = {
+
+    u'name':u'New Test',
+    u'phone_number':456,
+    u'sign':'New Test!!!'
+}
+
+doc_ref.document(u'two').set(data)
+
+for doc in docs:
+    pprint(f'{doc.id} => {doc.to_dict()}')
