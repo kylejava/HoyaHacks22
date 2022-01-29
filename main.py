150099cca8b838a0db7225426a3f03a0c2e55ebf
kyle java <42562960+kylejava@users.noreply.github.com>
2022-01-29 10:48:27 -0800

    added submit.html to Python Flask Routes

--- main.py
+++ main.py
@@ -20,5 +20,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/submit' , methods = ['GET' , 'POST'])
+def submit():
+    return render_template('submit.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
